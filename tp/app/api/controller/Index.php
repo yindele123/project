0814a6f560ddb692b1a88f31e159af83e9fc9394
981443737 <981443737>
2020-03-09 19:36:25 +0800

    no message

---
+++ tp/app/api/controller/Index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kezihang
+ * Date: 2020/2/25
+ * Time: 19:48
+ */
+namespace app\api\controller;
+use app\BaseController;
+
+class Index extends BaseController{
+
+}
--- tp/config/aliyun.php
+++ tp/config/aliyun.php
@@ -6,10 +6,5 @@
  * Time: 12:09
  */
 return[
-    'access_key_id'=>'LTAI4FjG4UnsQeQaZmNE4S9S',
-    'access_secret'=>'GFSVoGj8XdPHLU1oabYgE1pVDsh4JY',
-    'region_id'=>'cn-hangzhou',
-    'host'=>'dysmsapi.aliyuncs.com',
-    'sign_name'=>'便一宜',
-    'template_code'=>'SMS_173470333'
+
 ];
